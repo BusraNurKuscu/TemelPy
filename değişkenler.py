ae38f4a1ac168e7c2348f40a92e45ab585832644
BusraNurKuscu <busranurkuscu@gmail.com>
2021-01-01 12:31:48 +0000

    şartblokları(if)

---
+++ değişkenler.py
@@ -0,0 +1,23 @@
+haber="Haberiniz Olsun" #yorum satırı
+vade=12
+faizOrani=1.47
+faizOrani1=1.44
+faizOrani2=1.47
+
+
+print(haber)
+print(type(haber))
+print(type(vade))
+print(type(faizOrani)) 
+
+mesaj= "Hoşgeldin"
+musteriAdi="Büşra Nur"
+musteriSoyadi= "Kuşcu"
+print(mesaj+" "+musteriAdi+" "+musteriSoyadi)
+sonucMesaj=mesaj+" "+musteriAdi+" "+musteriSoyadi
+print(sonucMesaj)
+
+sayi1=20
+sayi2=21
+print(sayi1+sayi2)
+print("20"+"21")
--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-haber="Haberiniz Olsun" #yorum satırı
-vade=12
-faizOrani=1.47
-faizOrani1=1.44
-faizOrani2=1.47
+dolarDun=7.65
+dolarBugun=7.75
+
+if dolarDun>dolarBugun:
+  print("Azalış oku")
+if dolarDun<dolarBugun:
+  print("Artış oku")
+if dolarDun == dolarBugun:
+  print("Eşittir oku")
+
+ 
 
 
-print(haber)
-print(type(haber))
-print(type(vade))
-print(type(faizOrani)) 
 
-mesaj= "Hoşgeldin"
-musteriAdi="Büşra Nur"
-musteriSoyadi= "Kuşcu"
-print(mesaj+" "+musteriAdi+" "+musteriSoyadi)
-sonucMesaj=mesaj+" "+musteriAdi+" "+musteriSoyadi
-print(sonucMesaj)
 
-sayi1=20
-sayi2=21
-print(sayi1+sayi2)
-print("20"+"21")
